fb0499fa00ebb8afbf36cdcc9f5cf1641bf0e991
gbai <bgm1996@outlook.com>
2021-05-15 00:41:51 +0200

    UQ added

--- BaiPinns/EquationModels/DispersiveEquation/KdV.py
+++ BaiPinns/EquationModels/DispersiveEquation/KdV.py
@@ -7,14 +7,25 @@ time_dimensions = 1
 # Number of space dimensions
 space_dimensions = 1
 # Number of additional non-space and non-temporal dimensions (for UQ problem for instance)
-parameter_dimensions = 0
+parameter_dimensions = 3
 # Number of output dimensions
 output_dimensions = 1
 # Domain Extrema
 extrema_values = torch.tensor([[-1., 1.],  # Time t; single [-1., 1.]; double [-5., 5.]
                                [-10., 10.]])  # Space x; single [-10., 10.]; double [-15., 15.]
 # Additional variable to use here
-c = 3
+# c = 3
+
+# UQ single
+parameters_values = torch.tensor([[8.9, 9.1], # alpha
+                  [-0.1, 0.1], # beta
+                  [0.9, 1.1]]) # kappa
+parameter_dimensions = 3
+
+# UQ double
+# parameters_values = torch.tensor([[0.4, 0.6], # a
+#                   [0.9, 1.1]]) # b
+# parameter_dimensions = 2
 
 # val_range = [-1., 9.] # single
 # val_range = [-1., 6.] # double
@@ -34,6 +45,7 @@ def compute_res(network, x_f_train, space_dimensions, solid_object, computing_er
 
     x_f_train.requires_grad = True
     u = network(x_f_train).reshape(-1, )
+    sp = x_f_train.shape
     u_sq = 0.5 * u * u
     grad_u = torch.autograd.grad(u, x_f_train, grad_outputs=torch.ones(x_f_train.shape[0], ), create_graph=True)[0]
     grad_u_sq_x = torch.autograd.grad(u_sq, x_f_train, grad_outputs=torch.ones(x_f_train.shape[0], ), create_graph=True)[0][:, 1]
@@ -42,7 +54,11 @@ def compute_res(network, x_f_train, space_dimensions, solid_object, computing_er
     grad_u_xx = torch.autograd.grad(grad_u_x, x_f_train, grad_outputs=torch.ones(x_f_train.shape[0]), create_graph=True)[0][:, 1]
     grad_u_xxx = torch.autograd.grad(grad_u_xx, x_f_train, grad_outputs=torch.ones(x_f_train.shape[0]), create_graph=True)[0][:, 1]
 
-    residual = grad_u_t.reshape(-1, ) + grad_u_sq_x.reshape(-1, ) + grad_u_xxx.reshape(-1, )
+    # UQ single
+    kappa = x_f_train[:, 4].reshape(-1, )
+    residual = grad_u_t.reshape(-1, ) + grad_u_sq_x.reshape(-1, ) + kappa * grad_u_xxx.reshape(-1, )
+
+    # residual = grad_u_t.reshape(-1, ) + grad_u_sq_x.reshape(-1, ) + grad_u_xxx.reshape(-1, )
 
     return residual
 
@@ -59,15 +75,26 @@ def exact(inputs):
     x = inputs[:, 1]
 
     # KdV single soliton
-    u = torch.tensor(3 * c) / torch.cosh(np.sqrt(c) / 2 * (x - c * t)) ** 2
+    # u = torch.tensor(3 * c) / torch.cosh(np.sqrt(c) / 2 * (x - c * t)) ** 2
+    #UQ single
+    alpha = inputs[:, 2]
+    beta = inputs[:, 3]
+    kappa = inputs[:, 4]
+    u = beta + (alpha - beta) / torch.cosh(np.sqrt((alpha - beta) / (12 * kappa)) * (x - (beta + (alpha - beta) / 3) * t)) ** 2
+
 
     # KdV double soliton
     # a = torch.tensor(.5)
     # b = torch.tensor(1.)
+
+    # UQ double
+    # a = inputs[:, 2]
+    # b = inputs[:, 3]
     #
     # u = 6 * (b - a) \
     #     * (b / torch.sinh(torch.sqrt(0.5 * b) * (x - 2 * b * t)) ** 2 + a / torch.cosh(torch.sqrt(0.5 * a) * (x - 2 * a * t)) ** 2) \
     #     / (torch.sqrt(a) * torch.tanh(torch.sqrt(0.5 * a) * (x - 2 * a * t)) - torch.sqrt(b) / torch.tanh(torch.sqrt(0.5 * b) * (x - 2 * b * t))) ** 2
+
 
     return u.reshape(-1, 1)
 
@@ -82,9 +109,26 @@ def ub0(t):
     '''
     # Specify tipy of BC: "func" = "Dirichlet
     type_BC = ["func"]
+    # x0 = torch.full(size=(t.shape[0], 1), fill_value=extrema_values[1, 0], dtype=torch.double)
+
+    # UQ single
+    t0 = t[:, 0].reshape(-1, 1)  # !!
     x0 = torch.full(size=(t.shape[0], 1), fill_value=extrema_values[1, 0], dtype=torch.double)
-    inputs = torch.cat([t, x0], 1)
+    x1 = t[:, 1].reshape(-1, 1)
+    x2 = t[:, 2].reshape(-1, 1)
+    x3 = t[:, 3].reshape(-1, 1)
+    inputs = torch.cat([t0, x0, x1, x2, x3], 1)
+
+    # UQ double
+    # t0 = t[:, 0].reshape(-1, 1)  # !!
+    # x0 = torch.full(size=(t.shape[0], 1), fill_value=extrema_values[1, 0], dtype=torch.double)
+    # x1 = t[:, 1].reshape(-1, 1)
+    # x2 = t[:, 2].reshape(-1, 1)
+    # inputs = torch.cat([t0, x0, x1, x2], 1)
+
     out = exact(inputs)
+
+    # out = torch.full(size=(t.shape[0], 1), fill_value=0, dtype=torch.double)
     return out.reshape(-1, 1), type_BC
 
 
@@ -97,9 +141,27 @@ def ub1(t):
     Returns: the vector containing the BC at given inputs
     '''
     type_BC = ["func"]
+    # x0 = torch.full(size=(t.shape[0], 1), fill_value=extrema_values[1, 1], dtype=torch.double)
+
+    # UQ single
+    t0 = t[:, 0].reshape(-1, 1)  # !!
     x0 = torch.full(size=(t.shape[0], 1), fill_value=extrema_values[1, 1], dtype=torch.double)
-    inputs = torch.cat([t, x0], 1)
+    x1 = t[:, 1].reshape(-1, 1)
+    x2 = t[:, 2].reshape(-1, 1)
+    x3 = t[:, 3].reshape(-1, 1)
+    inputs = torch.cat([t0, x0, x1, x2, x3], 1)
+
+    # UQ double
+    # t0 = t[:, 0].reshape(-1, 1)  # !!
+    # x0 = torch.full(size=(t.shape[0], 1), fill_value=extrema_values[1, 1], dtype=torch.double)
+    # x1 = t[:, 1].reshape(-1, 1)
+    # x2 = t[:, 2].reshape(-1, 1)
+    # inputs = torch.cat([t0, x0, x1, x2], 1)
+
     out = exact(inputs)
+
+
+    # out = torch.full(size=(t.shape[0], 1), fill_value=0, dtype=torch.double)
     return out.reshape(-1, 1), type_BC
 
 
@@ -112,14 +174,31 @@ def ub2(t):
     Returns: the vector containing the BC at given inputs
     '''
     type_BC = ["der"]
+    # x0 = torch.full(size=(t.shape[0], 1), fill_value=extrema_values[1, 1], dtype=torch.double)
+    # inputs = torch.cat([t, x0], 1)
+    # UQ single
+    t0 = t[:, 0].reshape(-1, 1)  # !!
     x0 = torch.full(size=(t.shape[0], 1), fill_value=extrema_values[1, 1], dtype=torch.double)
-    inputs = torch.cat([t, x0], 1)
+    x1 = t[:, 1].reshape(-1, 1)
+    x2 = t[:, 2].reshape(-1, 1)
+    x3 = t[:, 3].reshape(-1, 1)
+    inputs = torch.cat([t0, x0, x1, x2, x3], 1)
+
+    # UQ double
+    # t0 = t[:, 0].reshape(-1, 1)  # !!
+    # x0 = torch.full(size=(t.shape[0], 1), fill_value=extrema_values[1, 1], dtype=torch.double)
+    # x1 = t[:, 1].reshape(-1, 1)
+    # x2 = t[:, 2].reshape(-1, 1)
+    # inputs = torch.cat([t0, x0, x1, x2], 1)
+
     inputs.requires_grad_(True)
 
     u = exact(inputs).reshape(-1, )
     grad_u = torch.autograd.grad(u, inputs, grad_outputs=torch.ones(inputs.shape[0], ), create_graph=True)[0]
 
     grad_u_x = grad_u[:, 1]
+
+    # grad_u_x = torch.full(size=(t.shape[0], 1), fill_value=0, dtype=torch.double)
 
     return grad_u_x.reshape(-1, 1), type_BC
 
@@ -142,7 +221,12 @@ def u0(x):
     '''
 
     # KdV single soliton
-    u0 = torch.tensor(3 * c) / torch.cosh(np.sqrt(c) / 2 * (x + c)) ** 2
+    # u0 = torch.tensor(3 * c) / torch.cosh(np.sqrt(c) / 2 * (x + c)) ** 2
+    # UQ single
+    # t0 = torch.full(size=(x.shape[0], 1), fill_value=extrema_values[0, 0], dtype=torch.float)
+    # inputs = torch.cat([t0, x], 1)
+    # u0 = exact(inputs)
+
 
     # KdV double soliton
     # a = torch.tensor(.5)
@@ -152,6 +236,11 @@ def u0(x):
     # u0 = 6 * (b - a) \
     #     * (b / torch.sinh(torch.sqrt(0.5 * b) * (x - 2 * b * t0)) ** 2 + a / torch.cosh(torch.sqrt(0.5 * a) * (x - 2 * a * t0)) ** 2) \
     #     / (torch.sqrt(a) * torch.tanh(torch.sqrt(0.5 * a) * (x - 2 * a * t0)) - torch.sqrt(b) / torch.tanh(torch.sqrt(0.5 * b) * (x - 2 * b * t0))) ** 2
+
+    # UQ single & double
+    t0 = torch.full(size=(x.shape[0], 1), fill_value=extrema_values[0, 0], dtype=torch.float)
+    inputs = torch.cat([t0, x], 1)
+    u0 = exact(inputs)
 
     return u0.reshape(-1, 1)
 
@@ -176,6 +265,9 @@ def compute_generalization_error(model, extrema, images_path=None):
     '''
     model.eval()
     test_inp = convert(torch.rand([100000, extrema.shape[0]]), extrema)
+    # UQ
+    test_inp1 = convert(torch.rand([100000, parameters_values.shape[0]]), parameters_values)
+    test_inp = torch.cat([test_inp, test_inp1], 1)
     Exact = (exact(test_inp)).numpy()
     test_out = model(test_inp).detach().numpy()
     assert (Exact.shape[1] == test_out.shape[1])
@@ -215,6 +307,18 @@ def plotting(model, images_path, extrema, solid):
     plt.grid(True, which="both", ls=":")
     for val, scale in zip(time_steps, scale_vec):
         plot_var = torch.cat([torch.tensor(()).new_full(size=(n, 1), fill_value=val), x], 1)
+
+        # UQ single
+        alpha = torch.full(size=(x.shape[0], 1), fill_value=9., dtype=torch.float)
+        beta = torch.full(size=(x.shape[0], 1), fill_value=0., dtype=torch.float)
+        kappa = torch.full(size=(x.shape[0], 1), fill_value=1., dtype=torch.float)
+        plot_var = torch.cat([plot_var, alpha, beta, kappa], 1)
+
+        # UQ double
+        # a = torch.full(size=(x.shape[0], 1), fill_value=0.5, dtype=torch.float)
+        # b = torch.full(size=(x.shape[0], 1), fill_value=1.0, dtype=torch.float)
+        # plot_var = torch.cat([plot_var, a, b], 1)
+
         plt.plot(x, exact(plot_var), 'b-', linewidth=2, label=r'Exact, $t=$' + str(val) + r'$s$', color=lighten_color('grey', scale), zorder=0)
         plt.scatter(plot_var[:, 1].detach().numpy(), model(plot_var).detach().numpy(), label=r'Predicted, $t=$' + str(val) + r'$s$', marker="o", s=14,
                     color=lighten_color('C0', scale), zorder=10)
